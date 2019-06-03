19f0ee7bb44db1b0e9cfab38a39447d494ee8396
Shahbaz Rezaei <36204315+shrezaei@users.noreply.github.com>
2019-06-03 11:14:04 -0700

    Update multitaskMasked.py

--- multitaskMasked.py
+++ multitaskMasked.py
@@ -38,6 +38,18 @@ for i in range(train_size):
 print("unmasked samples: ", str(np.sum(trainmask==1)/256))
 
 
+valData = np.load("valData.npy")
+valLabel = np.load("valLabel.npy")
+# testData = testData[:, -timestep*2:]
+# testLabel = testLabel[:, -timestep*2:]
+valData = valData[:, :timestep*2]
+valLabel = valLabel[:, :timestep*2]
+
+valLabel = valLabel.astype(int)
+valmask = np.ones((valLabel.shape[0], 256))
+valmask[:,:]=1
+
+
 testData = np.load("testData.npy")
 testLabel = np.load("testLabel.npy")
 # testData = testData[:, -timestep*2:]
@@ -71,6 +83,29 @@ for i in range(trainlabel.shape[0]):
     else:
         trainlabel[i, 1] = 4
 
+for i in range(valLabel.shape[0]):
+    #Categorizing Bandwidth
+    if valLabel[i, 0] < 10000:
+        valLabel[i, 0] = 1
+    elif valLabel[i, 0] < 50000:
+        valLabel[i, 0] = 2
+    elif valLabel[i, 0] < 100000:
+        valLabel[i, 0] = 3
+    elif valLabel[i, 0] < 1000000:
+        valLabel[i, 0] = 4
+    else:
+        valLabel[i, 0] = 5
+    #Categorizing Duration
+    if valLabel[i, 1] < 10:
+        valLabel[i, 1] = 1
+    elif valLabel[i, 1] < 30:
+        valLabel[i, 1] = 2
+    elif valLabel[i, 1] < 60:
+        valLabel[i, 1] = 3
+    else:
+        valLabel[i, 1] = 4
+
+
 for i in range(testLabel.shape[0]):
     #Categorizing Bandwidth
     if testLabel[i, 0] < 10000:
@@ -102,6 +137,14 @@ Y_train2[np.arange(train_size),trainlabel[:,1]-1] = 1
 Y_train3 = np.zeros((train_size,5))
 Y_train3[np.arange(train_size),trainlabel[:,2]-1] = 1
 
+val_size = valLabel.shape[0]
+Y_val1 = np.zeros((val_size,5))
+Y_val1[np.arange(val_size),valLabel[:,0]-1] = 1
+Y_val2 = np.zeros((val_size,4))
+Y_val2[np.arange(val_size),valLabel[:,1]-1] = 1
+Y_val3 = np.zeros((val_size,5))
+Y_val3[np.arange(val_size),valLabel[:,2]-1] = 1
+
 test_size = testLabel.shape[0]
 Y_test1 = np.zeros((test_size,5))
 Y_test1[np.arange(test_size),testLabel[:,0]-1] = 1
@@ -114,6 +157,7 @@ Y_test3[np.arange(test_size),testLabel[:,2]-1] = 1
 # testData = np.expand_dims(testData, axis=-1)
 trainData = trainData.reshape((trainData.shape[0], timestep, 2))
 testData = testData.reshape((testData.shape[0], timestep, 2))
+valData = valData.reshape((valData.shape[0], timestep, 2))
 
 def base_model():
 
@@ -155,5 +199,8 @@ def base_model():
 model = base_model()
 
 model.fit([trainData,trainmask], [Y_train1, Y_train2, Y_train3],
-          validation_data = ([testData, testmask], [Y_test1, Y_test2, Y_test3]),
+          validation_data = ([valData, valmask], [Y_val1, Y_val2, Y_val3]),
           batch_size = 64, epochs = 20, verbose = True, shuffle = True)
+
+result = model.evaluate([testData, testmask], [Y_test1, Y_test2, Y_test3])
+print(result)
